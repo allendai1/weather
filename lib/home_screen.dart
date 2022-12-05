0bf24e85c42add3a06ea7577d48879822ee4e0f5
ALLEN DAI <allen.dai001@umb.edu>
2022-12-04 23:15:31 -0500

    .

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:at_client_mobile/at_client_mobile.dart';
 import 'package:at_app_flutter/at_app_flutter.dart' show AtEnv;
 import 'package:at_utils/at_logger.dart' show AtSignLogger;
-import 'package:path_provider/path_provider.dart' show getApplicationSupportDirectory;
+import 'package:path_provider/path_provider.dart'
+    show getApplicationSupportDirectory;
 
 // * Once the onboarding process is completed you will be taken to this screen
 class HomeScreen extends StatelessWidget {
@@ -12,29 +13,35 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // * Getting the AtClientManager instance to use below
+    final String atSignPico = "@distinctiveblue";
+    final String key = "temperature";
+    AtClientManager atClientManager = AtClientManager.getInstance();
+    final AtClient atClient = atClientManager.atClient;
+    final AtKey atKey = AtKey.public(
+        key, namespace: "group5", sharedBy: atSignPico).build();
+    int temp;
 
-
-
+    void getWeather() async {
+      GetRequestOptions options = GetRequestOptions();
+      options.bypassCache = true;
+      AtValue atValue = await atClient.get(atKey, getRequestOptions: options);
+      final String value = atValue.value;
+      print(value);
+    }
     return Scaffold(
       appBar: AppBar(
-        title: const Text('What\'s my current @sign?'),
+        title: const Text('Test'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            final String atSignPico = "@distinctiveblue";
-            final String key = "test";
-            AtClientManager atClientManager = AtClientManager.getInstance();
-            final AtClient atClient = atClientManager.atClient;
-            final AtKey atKey = AtKey.public(key, namespace: "group5", sharedBy: atSignPico).build();
-            // final AtValue atValue = await atClient.get(atKey);
-            // final String value = atValue.value;
-            print("test!");
-
-
-          },
-          child: const Text("Test!"),
-        )
+          child: [ElevatedButton(
+            onPressed: getWeather,
+            child: const Text("Get weather!"),
+          ),
+            SizedBox(
+              width: 18,
+              child: Text('$temp'),
+            )
+          ]
       ),
     );
   }
